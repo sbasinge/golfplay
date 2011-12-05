579421cf06a75e1ce52b40ac55dabc9c96e94da5
sbasinge <sbasinge@gmail.com>
2011-12-05 16:21:54 -0500

    latest updates

--- app/controllers/Clubs.java
+++ app/controllers/Clubs.java
@@ -1,9 +1,23 @@
 package controllers;
 
+import models.User;
+import play.mvc.Before;
 import play.mvc.With;
 
 
 @With(Secure.class)
 public class Clubs extends CRUD {
+
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byUsername", Security.connected()).first();
+            renderArgs.put("user", user.name);
+        }
+    }
+ 
+    public static void index() {
+        render();
+    }
 
 }
