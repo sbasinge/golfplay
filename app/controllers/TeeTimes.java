e07fa0cb9a0af1066dd8694cd29f2796c54a6598
Scott Basinger <sbasinge@gmail.com>
2011-12-19 07:45:05 -0500

    Start teetime pages

--- app/controllers/TeeTimes.java
+++ app/controllers/TeeTimes.java
@@ -1,9 +1,46 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.TeeTime;
+import play.data.validation.Valid;
 import play.mvc.With;
 
 
 @With(Secure.class)
-public class TeeTimes extends CRUD {
+public class TeeTimes extends Application {
+
+	public static void list() {
+		String selectedClub = (String) renderArgs.get("selectedClub");
+		//TODO should get teetimes for current club only.
+		List<TeeTime> teeTimes = new ArrayList<TeeTime>();
+		teeTimes = TeeTime.all().fetch();
+		render(teeTimes);
+	}
+	
+    public static void edit(Long id) {
+    	TeeTime teeTime = TeeTime.findById(id);
+        render(teeTime);
+    }
+
+    public static void view(Long id) {
+    	TeeTime teeTime = TeeTime.findById(id);
+        render(teeTime);
+    }
+
+    public static void delete(Long id) {
+    	TeeTime teeTime = TeeTime.findById(id);
+    	teeTime.delete();
+        list();
+    }
+
+    public static void save(@Valid TeeTime teeTime) {
+    	teeTime.save();
+    }
+
+    public static void cancel() {
+    	list();
+    }
 
 }
