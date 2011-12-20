ee117b24e4ede94cadc09a86b1d4038a463f51ed
Scott Basinger <sbasinge@gmail.com>
2011-12-20 15:38:12 -0500

    Teetime edit and view working

--- app/controllers/TeeTimes.java
+++ app/controllers/TeeTimes.java
@@ -3,7 +3,9 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Score;
 import models.TeeTime;
+import models.TeeTimeParticipant;
 import play.Logger;
 import play.data.validation.Valid;
 import play.mvc.With;
@@ -39,8 +41,31 @@ public class TeeTimes extends Application {
 
     public static void save(@Valid TeeTime teeTime) {
     	String[] grossScores = params.getAll("grossScore");
+    	String[] adjustedScores = params.getAll("adjustedScore");
     	Logger.info("TeeTime to save is %s",teeTime);
+    	int i=0;
+    	for (TeeTimeParticipant participant: teeTime.participants) {
+    		if (participant.score == null) {
+    			Score score = new Score();
+    			score.adjustedScore = Integer.parseInt(adjustedScores[i]);
+    			score.date = teeTime.date;
+    			score.grossScore = Integer.parseInt(grossScores[i]);
+    			score.teeSet = teeTime.teeSet;
+    			score.teeTime = teeTime;
+    			score.user = participant.user;
+    			participant.score = score;
+    			score.save();
+    		} else {
+    			Score score = participant.score;
+    			score.adjustedScore = Integer.parseInt(adjustedScores[i]);
+    			score.date = teeTime.date;
+    			score.grossScore = Integer.parseInt(grossScores[i]);
+    			score.save();
+    		}
+    		i++;
+    	}
     	teeTime.save();
+    	list();
     }
 
     public static void cancel() {
