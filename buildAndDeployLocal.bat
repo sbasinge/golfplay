8d51cfa92a62f7cfa59a25fdd23720971d55c581
Scott Basinger <sbasinge@gmail.com>
2011-12-20 13:54:16 -0500

    Update jquery to 1.7.1

---
+++ buildAndDeployLocal.bat
@@ -0,0 +1,3 @@
+play war -o /jboss-as-7.0.2.Final/standalone/deployments/golfplay.war
+type NUL > /jboss-as-7.0.2.Final/standalone/deployments/golfplay.war.dodeploy
+touch /jboss-as-7.0.2.Final/standalone/deployments/golfplay.war.dodeploy
